6576ad5bf660ac71930077d6974f4034a36c38c0
John Mclaren <john@johnscs.com>
2019-06-10 20:35:58 -0400

    Use personal token for api

--- Main.bat
+++ Main.bat
@@ -1,1 +1,13 @@
-"%~dp0\AutoIT3.exe" /AutoIt3ExecuteScript "%~dp0Main.au3"
+@echo off
+echo Checking administrative permissions...
+
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+	echo Success: Administrative permissions confirmed.
+) else (
+        echo Warning: No administrative permissions.
+)
+
+set /p params="Enter Parameters (or leave blank): "
+"%~dp0\AutoIT3.exe" /AutoIt3ExecuteScript "%~dp0\Main.au3" %params%
