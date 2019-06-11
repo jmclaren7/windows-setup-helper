ff000c349d8db3455c793f594388c094a36969fa
John Mclaren <john@johnscs.com>
2019-06-11 00:34:52 -0400

    remove double slash

--- Main.bat
+++ Main.bat
@@ -1,7 +1,6 @@
 @echo off
 echo Checking administrative permissions...
 
-
 net session >nul 2>&1
 if %errorLevel% == 0 (
 	echo Success: Administrative permissions confirmed.
@@ -10,4 +9,4 @@ if %errorLevel% == 0 (
 )
 
 set /p params="Enter Parameters (or leave blank): "
-"%~dp0\AutoIT3.exe" /AutoIt3ExecuteScript "%~dp0\Main.au3" %params%
+"%~dp0AutoIT3.exe" /AutoIt3ExecuteScript "%~dp0Main.au3" %params%
