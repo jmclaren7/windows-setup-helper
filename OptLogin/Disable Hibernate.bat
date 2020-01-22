80c0a9df8f544c1a00fe7b04289fb7c7c2998d5e
John Mclaren <john@johnscs.com>
2020-01-22 02:54:20 -0500

    rename and expand basic power

--- OptLogin/Disable Hibernate.bat
+++
@@ -1,1 +0,0 @@
-powercfg.exe /h off
---
+++ OptLogin/Workstation Power.bat
@@ -0,0 +1,5 @@
+powercfg /h off
+powercfg /change standby-timeout-ac 0
+powercfg /change standby-timeout-dc 0
+powercfg /change monitor-timeout-ac 60
+powercfg /change monitor-timeout-dc 10
