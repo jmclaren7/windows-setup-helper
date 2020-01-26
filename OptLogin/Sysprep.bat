530388a0baf928344935ba7d9d2f7039580121aa
John Mclaren <john@johnscs.com>
2020-01-25 22:47:04 -0500

    Update Sysprep.bat

--- OptLogin/Sysprep.bat
+++ OptLogin/Sysprep.bat
@@ -4,7 +4,7 @@ setlocal
 SET /P AREYOUSURE=Are you sure you want to run sysprep and shutdown the system (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
-C:\Windows\System32\Sysprep\sysprep.exe /oobe /shutdown /generalize /unattend:C:\Windows\IT\autounattend.xml
+start C:\Windows\System32\Sysprep\sysprep.exe /oobe /shutdown /generalize /unattend:C:\Windows\IT\autounattend.xml
 
 :END
 endlocal
