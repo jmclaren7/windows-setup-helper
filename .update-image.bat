85f054d09105f9a3ad59ca35aea73342212f4617
John Mclaren <john@johnscs.com>
2021-04-01 21:58:16 -0400

    Update .update-image.bat

--- .update-image.bat
+++ .update-image.bat
@@ -1,8 +1,9 @@
+@ECHO OFF
 set image-root=H:\Windows 10 Images\2009
 
 del "%image-root%\Auto-saved*.xml"
 del "%image-root%\NTLite.log"
 
-robocopy "%~dp0\" "%image-root%\sources\$OEM$\$$\IT" /mir /xd .git
+robocopy "%~dp0\" "%image-root%\sources\$OEM$\$$\IT" /mir /njs /njh /xd .git
 
 pause
