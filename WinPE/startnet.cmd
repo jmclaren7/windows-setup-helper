942189769b6e97d819c1444e0915a7f87e3c89c7
John Mclaren <john@johnscs.com>
2021-08-15 14:21:20 -0400

    rework winpe boot

---
+++ WinPE/startnet.cmd
@@ -0,0 +1,8 @@
+reg.exe add HKCU\Console /v ForceV2 /t REG_DWORD /d 0 /f
+set target=:\sources\$OEM$\$$\IT\WinPE\winpehelper.exe
+
+for %%d in (c d e f g h i j k l m n o p q r s t u v w x y z) do (
+	if exist "%%d%target%" (
+		"%%d%target%"
+	)
+)
