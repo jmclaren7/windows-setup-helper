0c28fa3d7c9ef41c1757c2290e577431e40bd3cf
John Mclaren <john@johnscs.com>
2022-12-11 15:57:22 -0500

    Update Delete Disk 0 Partitions.bat

--- Helper/Tools/Delete Disk 0 Partitions.bat
+++ Helper/Tools/Delete Disk 0 Partitions.bat
@@ -1,13 +1,13 @@
 @echo off
-setlocal
-:PROMPT
-SET /P AREYOUSURE=Are you sure (Y/[N])?
-IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+echo.
+echo WANRING, DATA LOSS IF YOU CONTINUE
+echo.
+choice /C yn /N /M "Delete Partitions? (y/N)"
+if /I "%errorlevel%" neq "1" goto end
 
 (echo Select Disk 0
 echo clean
 )  | diskpart
 
-
-:END
-endlocal
+:end
