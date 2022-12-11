795fae807c9ba8744d4cef0c51f15335f98c0f0e
John Mclaren <john@johnscs.com>
2022-12-10 23:04:42 -0500

    Update .update-image.bat

--- .update-image.bat
+++ .update-image.bat
@@ -1,25 +1,29 @@
 @ECHO OFF
 Call :Admin
 
-set additions-root=H:\Windows 10 Images
+REM =====================================================================
+REM This is the location NTLite uses when mounting an image
 set target=%TEMP%\NLTmpMnt01
 
-del "%image-root%\Auto-saved*.xml"
-del "%image-root%\NTLite.log"
+REM Remove the Helper folder to start fresh
+rmdir /s /q "%target%\Helper"
 
-robocopy "%~dp0\Helper" "%target%\Helper" /mir
-robocopy "%~dp0\Windows" "%target%\Windows" /e /xx
+REM Copy files from repository to WinPE image
+robocopy "%~dp0\Helper" "%target%\Helper" /mir /NFL /NDL
+robocopy "%~dp0\Windows" "%target%\Windows" /e /xx /NFL /NDL
 
-robocopy "%additions-root%\Additions" "%target%" /e /xx
-robocopy "%additions-root%\Additions-Macrium" "%target%" /e /xx
+REM Copy additional files to the image (Make sure to include required files like Autoit.exe)
+robocopy "H:\Windows 10 Images\Additions" "%target%" /e /xx /NFL /NDL
+robocopy "H:\Windows 10 Images\Additions-Macrium" "%target%" /e /xx /NFL /NDL
+
+REM Remove extra files from image
+del "%target%\Auto-saved*.xml"
+del "%target%\NTLite.log"
 
 pause
 exit
 
-
-
-
-
+REM =====================================================================
 :Admin
 echo Administrative permissions required. Detecting permissions...
 net session >nul 2>&1
