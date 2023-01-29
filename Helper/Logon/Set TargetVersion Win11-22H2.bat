f1d5b448377d3c53de96baadb766813b83b13cad
John Mclaren <john@johnscs.com>
2023-01-29 16:35:11 -0500

    Win11 22H2

--- Helper/Logon/Set TargetVersion Win11-22H2.bat
+++ Helper/Logon/Set TargetVersion Win11-22H2.bat
@@ -1,4 +1,4 @@
 @echo off
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v TargetReleaseVersion /t REG_DWORD /d 00000001 /f
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v ProductVersion /d "Windows 10" /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v ProductVersion /d "Windows 11" /f
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v TargetReleaseVersionInfo /d "22H2" /f
