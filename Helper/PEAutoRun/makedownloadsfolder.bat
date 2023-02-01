fa7538d9c60b8c03ebac052c8f559779bbb41e17
John Mclaren <john@johnscs.com>
2023-02-01 00:06:31 -0500

    more profile folders, rename

--- Helper/PEAutoRun/makedownloadsfolder.bat
+++
@@ -1,1 +0,0 @@
-mkdir "%USERPROFILE%\Downloads"
---
+++ Helper/PEAutoRun/makeprofilefolders.bat
@@ -0,0 +1,4 @@
+REM this is to help with programs that open an explorer dialog which navigates to a specific folder
+mkdir "%USERPROFILE%\Downloads"
+mkdir "%USERPROFILE%\Documents"
+mkdir "%USERPROFILE%\Desktop"
