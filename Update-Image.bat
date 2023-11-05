16cb97f18cc017d72c35316f3fe8f2f114128c68
John Mclaren <john@johnscs.com>
2023-11-04 23:21:01 -0400

    add update-image.bat

--- Update-Image.bat
+++ Update-Image.bat
@@ -1,36 +1,37 @@
 @ECHO OFF
 Call :Admin
+cls
 
 REM =====================================================================
-REM This is the location NTLite uses when mounting an image
+REM The location where the WIM image is mounted, NTLite uses %TEMP%\NLTmpMnt01
 set target=%TEMP%\NLTmpMnt01
 
-REM Remove the Helper folder to start fresh
-rmdir /s /q "%target%\Helper"
-
-REM Copy files from repository to WinPE image
+REM Copy files from repository to mount location
+REM The first copy operation uses /mir to remove any left over files from old copy operations
 robocopy "%~dp0\Helper" "%target%\Helper" /mir /NFL /NDL
-robocopy "%~dp0\Windows" "%target%\Windows" /e /xx /NFL /NDL
+robocopy "%~dp0\Windows" "%target%\Windows" /e /NFL /NDL
 
 REM Copy additional files to the image (Make sure to include required files like Autoit.exe)
-robocopy "H:\Windows Images\Additions" "%target%" /e /xx /NFL /NDL
-robocopy "H:\Windows Images\Additions-Macrium" "%target%" /e /xx /NFL /NDL
+robocopy "H:\Windows Images\Additions" "%target%" /e /NFL /NDL
+robocopy "H:\Windows Images\Additions-Macrium" "%target%" /e /NFL /NDL
 
 REM Remove extra files from image
 del "%target%\Auto-saved*.xml"
 del "%target%\NTLite.log"
 
+
 pause
 exit
 
 REM =====================================================================
+
 :Admin
 echo Administrative permissions required. Detecting permissions...
 net session >nul 2>&1
 if %errorLevel% == 0 (
-  echo Success: Administrative permissions confirmed, continuing.
+  echo Success: Elevated permissions confirmed, continuing.
 ) else (
-  echo Failure: Not running with elevated permissions, please restart tool.
+  echo Failure: Not running with elevated permissions, please restart script.
   pause >nul
   exit
 )
