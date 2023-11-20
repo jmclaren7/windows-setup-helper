3ee1feaf382b832f81d4aa58172ca76141b967ff
John Mclaren <john@johnscs.com>
2023-11-19 21:54:01 -0500

    add special case for system autorun, optimizations

--- Helper/Logon/.Autorun.ps1
+++ Helper/Logon/.Autorun.ps1
@@ -4,70 +4,63 @@ $MyInvocation.MyCommand.Path | Split-Path | Push-Location
 
 Start-Sleep 2
 
-$Like = @(
-    '*.bat',
-    '*.ps1',
-    '*.exe',
-    '*.reg',
-    '*.cmd')
-$NotLike = @(
-    '.*')
+# Safety check to make sure we're running on a new OS install, if not don't run anything or make changes
+$os = Get-WmiObject -Class Win32_OperatingSystem
+$installAge = [DateTime]::Now - $os.ConvertToDateTime($os.InstallDate)
+if ($installAge.Days -gt 1) { $Execute = $false } else { $Execute = $true }
 
-$Run = @()
+# Set the file types to run
+$FileTypes = ('*.bat', '*.ps1', '*.exe', '*.reg', '*.cmd')
 
-foreach($File in Get-ChildItem){
-    $Add = $False
+# Get a list of files from the working directory
+$Run = Get-ChildItem -Include $FileTypes -Exclude '.*' -Recurse -Depth 0
 
-    foreach($pattern in $Like){
-        if($File.Name -like $pattern){ 
-            $Add = $True
-            Break
-        }
-    }
-
-    foreach($pattern in $NotLike){
-        if($File.Name -like $pattern){ 
-            $Add = $False 
-            Break
-        }
-    }
-
-    If ($Add){ 
-        $Run += $File.FullName 
-    }
+# Filter out system files unless specified
+if ($args[0] -eq 'system') {
+    $Run = $Run | Where-Object { $_.Name.Substring($_.Name.Length - 9) -like '*.sys.*' }
+}
+else {
+    $Run = $Run | Where-Object { $_.Name.Substring($_.Name.Length - 9) -notlike '*.sys.*' }
 }
 
+# Run each file in the list
 $Run | ForEach-Object {
-    $Item = Get-ChildItem $_
-    $Item.Name
+    $_.Name
 
-	# Wait for Windows Installer to be available
-	for($i = 0; $i -lt 12; $i++){
-		try{
-			$Mutex = [System.Threading.Mutex]::OpenExisting("Global\_MSIExecute")
-			$Mutex.Dispose()
-			"Windows Installer is busy, waiting..."
-		}catch{
+    Start-Sleep 2
+
+    # Wait for Windows Installer to be available
+    for ($i = 0; $i -lt 12; $i++) {
+        try {
+            $Mutex = [System.Threading.Mutex]::OpenExisting("Global\_MSIExecute")
+            $Mutex.Dispose()
+            "Windows Installer is busy, waiting..."
+        }
+        catch {
             Start-Sleep 2
-			Break
-		}
+            Break
+        }
 		
-		Start-Sleep 5
-	}
-
-	# Run the item
-    if($Item.Extension -eq ".reg"){
-        $Proc = Start-Process reg.exe -ArgumentList "import `"$($Item.FullName)`"" -PassThru
-    }elseif($Item.Extension -eq ".ps1"){
-        $Proc = Start-Process powershell.exe -ArgumentList "-file `"$($Item.FullName)`"" -PassThru
-    }else{
-        $Proc = Start-Process $Item.FullName -PassThru
+        Start-Sleep 4
     }
 
-    $Proc | Wait-Process -Timeout 20 -ErrorAction SilentlyContinue
-	if($Proc.HasExited -eq $False){
-		"Process has not finished, trying to continue..."
-	}
+    # Run the file
+    if ($Execute) { 
+        if ($_.Extension -eq ".reg") {
+            $Proc = Start-Process reg.exe -ArgumentList "import `"$($_.FullName)`"" -PassThru
+        }
+        elseif ($_.Extension -eq ".ps1") {
+            $Proc = Start-Process powershell.exe -ArgumentList "-file `"$($_.FullName)`"" -PassThru
+        }
+        else {
+            $Proc = Start-Process $_.FullName -PassThru
+        }
+
+        $Proc | Wait-Process -Timeout 20 -ErrorAction SilentlyContinue
+        if ($Proc.HasExited -eq $False) {
+            "Process still running, continuing anyway..."
+        }
+    }
 }
 
 "Complete, displaying prompt..."
@@ -76,14 +69,13 @@ Start-Sleep 2
 
 # 1=Ok/Cancel 32=Question Mark 4096=Always on top (Undocumented?)
 $Wscript_Shell = New-Object -ComObject "Wscript.Shell"
-$MsgBox = $Wscript_Shell.Popup("Logon scripts finished, removing scripts in 2 minutes, press 'ok' to remove now or 'cancel' to stop automatic removal.", 120, "Setup Helper", 1+32+4096)
+$MsgBox = $Wscript_Shell.Popup("Logon scripts finished, removing scripts in 2 minutes, press 'ok' to remove now or 'cancel' to stop automatic removal.", 120, "Setup Helper", 1 + 32 + 4096)
 
-If ($MsgBox -ne 1 -AND $MsgBox -ne -1) { Exit }
+if ($MsgBox -eq 1 ) { 
+    "Deleting script folder"
+    Set-Location ..
+    If ($Execute) { Remove-Item -LiteralPath $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Recurse -Force -ErrorAction SilentlyContinue }
 
-"Deleting script folder"
-Set-Location ..
-Remove-Item -LiteralPath $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Recurse -Force -ErrorAction SilentlyContinue
-
-if(-Not $?){
-    Powershell.exe -NoLogo
 }
+
+Powershell.exe -NoLogo
