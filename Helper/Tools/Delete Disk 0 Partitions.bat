1ae4554917804b0f20c021ee6631bdcac7067e21
John Mclaren <john@johnscs.com>
2023-11-19 21:24:56 -0500

    Update Delete Disk 0 Partitions.bat

--- Helper/Tools/Delete Disk 0 Partitions.bat
+++ Helper/Tools/Delete Disk 0 Partitions.bat
@@ -4,10 +4,9 @@ echo.
 echo WANRING, DATA LOSS IF YOU CONTINUE
 echo.
 SET /P input=Delete Partitions? (y/N)?
-IF /I "%input%" NEQ "Y" GOTO end
+IF /I "%input%" NEQ "Y" exit
 
-(echo Select Disk 0
+(
+echo Select Disk 0
 echo clean
-)  | diskpart
-
-:end
+) | diskpart
