416b6ce076b2b0006ef31449af64aeb54b511d12
John Mclaren <john@johnscs.com>
2024-03-06 12:09:01 -0500

    fix start windows update for win11 and make it selected by default

--- Helper/Logon/Start Windows Updates.bat
+++ Helper/Logon/Start Windows Updates.bat
@@ -1,3 +1,9 @@
-explorer.exe ms-settings:windowsupdate-action
-timeout 6
-explorer.exe ms-settings:windowsupdate-action
+usoclient StartInteractiveScan ScanInstallWait
+
+
+REM Other related commands, not working or not needed
+REM explorer.exe ms-settings:windowsupdate-action
+REM (New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow()
+REM $updateSession = new-object -com "Microsoft.Update.Session"; $updates=$updateSession.CreateupdateSearcher().Search($criteria).Updates
+REM wuauclt /reportnow
+REM wuauclt /UpdateNow
