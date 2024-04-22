44cef99749db68c58e7c944344ad40f8a500740a
John Mclaren <john@johnscs.com>
2024-04-22 00:45:14 -0400

    disable d3d for vnc, remove workaround for console issue
    Disabling d3d means the console fix is no longer needed, hookldr.exe is no longer needed either (might never have been needed). Disabling d3d was prompted by a black screen issue with vnc when DPI changes were introduced to the build script.

--- Helper/PEAutoRun/.Set v2 Console.bat
+++ Helper/PEAutoRun/.Set v2 Console.bat
@@ -1,2 +1,3 @@
+REM This issue is resolved by disabling D3D in VNC, this file is disabled (.filename) but left for posterity
 REM The default (V2) console window can't be controlled over VNC for an unknown reason so use V1
 reg.exe add HKCU\Console /v ForceV2 /t REG_DWORD /d 0 /f
