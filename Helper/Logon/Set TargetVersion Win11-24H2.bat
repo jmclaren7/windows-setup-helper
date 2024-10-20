9653508582e18bdf7d3677173d395f7798b1d396
John Mclaren <john@johnscs.com>
2024-10-20 14:32:32 -0400

    change example script to 24h2

--- Helper/Logon/Set TargetVersion Win11-24H2.bat
+++ Helper/Logon/Set TargetVersion Win11-24H2.bat
@@ -3,4 +3,5 @@ REM An example of how to set a target Windows version to avoid major Windows upd
 
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v TargetReleaseVersion /t REG_DWORD /d 00000001 /f
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v ProductVersion /d "Windows 11" /f
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v TargetReleaseVersionInfo /d "23H2" /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v TargetReleaseVersionInfo /d "24H2" /f
+pause
