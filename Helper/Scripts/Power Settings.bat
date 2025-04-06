7d2e171399290af9a7d1b0f6f0c531eafd4c4ed4
John Mclaren <john@johnscs.com>
2025-04-05 23:57:32 -0400

    Update Power Settings.bat

--- Helper/Scripts/Power Settings.bat
+++ Helper/Scripts/Power Settings.bat
@@ -2,7 +2,9 @@
 REM Disables Windows fast startup feature
 
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t reg_dword /d 0 /f
-powercfg /change standby-timeout-ac 0
-powercfg /change standby-timeout-dc 20
 powercfg /change monitor-timeout-ac 30
 powercfg /change monitor-timeout-dc 10
+powercfg /change standby-timeout-ac 0
+powercfg /change standby-timeout-dc 20
+powercfg /change hibernate-timeout-ac 0
+powercfg /change hibernate-timeout-dc 30
