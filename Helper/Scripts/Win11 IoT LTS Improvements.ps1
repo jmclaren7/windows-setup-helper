33982d2015be642e0aa527e6ee1f84d1e4c32138
John McLaren <john@johnscs.com>
2026-01-01 15:55:47 -0500

    Create Win11 IoT LTS Improvements.ps1

---
+++ Helper/Scripts/Win11 IoT LTS Improvements.ps1
@@ -0,0 +1,7 @@
+$progressPreference = 'silentlyContinue'
+Write-Host "Installing WinGet PowerShell module from PSGallery..."
+Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
+Repair-WinGetPackageManager
+Write-Host "Done."
