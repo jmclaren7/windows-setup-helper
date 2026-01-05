3cdcc387a50c5f055054287a5ce3c48e1b90fd15
John McLaren <john@johnscs.com>
2026-01-05 11:11:29 -0500

    Win11 IoT LTS script

--- Helper/Scripts/Win11 IoT LTS.ps1
+++ Helper/Scripts/Win11 IoT LTS.ps1
@@ -4,4 +4,6 @@ Install-PackageProvider -Name NuGet -Force | Out-Null
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
 Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
 Repair-WinGetPackageManager
-Write-Host "Done."
+Write-Host "Installing applications via WinGet..."
+$Apps = "Microsoft.WindowsTerminal,Microsoft.PowerShell"
+$Apps -split ',' | ForEach-Object {"installing: $_"; winget install --scope Machine --accept-package-agreements --accept-source-agreements --silent -e --id $_.Trim()}
