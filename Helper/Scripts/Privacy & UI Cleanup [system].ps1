e1021c2d5fb9b2cfe0e1deb38ee674a80f17d85a
John McLaren <john@johnscs.com>
2026-03-10 15:51:36 -0400

    fix registry paths

--- Helper/Scripts/Privacy & UI Cleanup [system].ps1
+++ Helper/Scripts/Privacy & UI Cleanup [system].ps1
@@ -4,7 +4,7 @@
 $DefaultUserHive = "HKLM\default-user"
 reg.exe load $DefaultUserHive "C:\Users\Default\NTUSER.DAT"
 
-$cdmPath = "$DefaultUserHive\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
+$cdmPath = "Registry::$DefaultUserHive\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
 $cdmValues = @{
     ContentDeliveryAllowed          = 0
     FeatureManagementEnabled        = 0
@@ -24,7 +24,7 @@ $cdmValues = @{
 }
 foreach ($name in $cdmValues.Keys) { Set-ItemProperty -Path $cdmPath -Name $name -Value $cdmValues[$name] -Type DWord -Force }
 
-$explorerPath = "$DefaultUserHive\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$explorerPath = "Registry::$DefaultUserHive\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 $cdmValues = @{
     ShowSyncProviderNotifications = 0
     ShowMeTheWindowsWelcomeExperience = 0
@@ -36,12 +36,12 @@ $cdmValues = @{
 }
 foreach ($name in $cdmValues.Keys) { Set-ItemProperty -Path $explorerPath -Name $name -Value $cdmValues[$name] -Type DWord -Force }
 
-$runPath = "$DefaultUserHive\Software\Microsoft\Windows\CurrentVersion\Run"
+$runPath = "Registry::$DefaultUserHive\Software\Microsoft\Windows\CurrentVersion\Run"
 Remove-ItemProperty -Path $runPath -Name OneDriveSetup -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path $runPath -Name OneDrive -ErrorAction SilentlyContinue
 
 reg.exe unload $DefaultUserHive
 
-$cloudPath = "HKLM:\Software\Policies\Microsoft\Windows\CloudContent"
+$cloudPath = "Registry::HKLM\Software\Policies\Microsoft\Windows\CloudContent"
 New-Item -Path $cloudPath -Force | Out-Null
 Set-ItemProperty -Path $cloudPath -Name DisableWindowsConsumerFeatures -Value 1 -Type DWord -Force
